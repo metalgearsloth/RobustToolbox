60da61702adace410b62a4e8327f37a451834c3f
metalgearsloth <comedian_vs_clown@hotmail.com>
2022-01-12 12:48:18 +1100

    More work

--- Robust.Shared/GameObjects/Systems/MapManagerSystem.MergeSplit.cs
+++ Robust.Shared/GameObjects/Systems/MapManagerSystem.MergeSplit.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using Robust.Shared.Console;
+using Robust.Shared.IoC;
 using Robust.Shared.Map;
 using Robust.Shared.Maths;
 using Robust.Shared.Utility;
@@ -42,6 +43,7 @@ public class MapManagerSystem : EntitySystem
         var bodyTwo = EntityManager.GetComponent<PhysicsComponent>(gridTwo.GridEntityId);
         GridId gridId;
 
+        // TODO: Tile count would be way better.
         // Find larger grid
         if (bodyOne.Mass > bodyTwo.Mass)
         {
@@ -58,14 +60,43 @@ public class MapManagerSystem : EntitySystem
         // Update tiles on grid one.
         var gridTwoTiles = new List<(Vector2i, Tile)>();
         // TODO: Maps can easily store how many tiles they have to pre-allocate.
+        // TODO: Probably need a struct enumerator
         foreach (var tile in gridTwo.GetAllTiles())
         {
-            var adjustedIndex = tile.GridIndices + origin;
+            var adjustedIndex = GetAdjustedIndex(tile.GridIndices, direction);
 
             gridTwoTiles.Add((adjustedIndex, tile.Tile));
         }
 
+        // Do the tiles all at once to avoid continuously regenerating collision
         gridOne.SetTiles(gridTwoTiles);
+
+        // Now transfer entities across
+        var gridOneComp = EntityManager.GetComponent<MapGridComponent>(gridOne.GridEntityId);
+        var gridTwoXform = EntityManager.GetComponent<TransformComponent>(gridTwo.GridEntityId);
+
+        // TODO: This is probably going to be the most expensive bit with slamming entity events all over the place.
+        // and general code quality issues.
+        foreach (var child in gridTwoXform.ChildEntities)
+        {
+            var childXform = EntityManager.GetComponent<TransformComponent>(child);
+
+            // Need to do local position relative to the specified grid-one origin PLUS consider rotation too
+            var newLocalPos = GetNewLocalPos(childXform.LocalPosition, origin, direction);
+            childXform.LocalPosition = newLocalPos;
+
+            if (childXform.Anchored)
+            {
+                childXform.Anchored = false;
+                gridOneComp.AnchorEntity(childXform);
+            }
+            else
+            {
+                childXform.AttachParent(gridOne.GridEntityId);
+            }
+        }
+
+        DebugTools.Assert(gridTwoXform.ChildCount == 0);
 
         DebugTools.Assert(EntityManager.GetComponent<TransformComponent>(gridTwo.GridEntityId).ChildCount == 0);
         EntityManager.DeleteEntity(gridTwo.GridEntityId);
@@ -73,16 +104,20 @@ public class MapManagerSystem : EntitySystem
         return gridId;
     }
 
-    private Vector2i GetAdjustedIndex(Vector2i index, Direction direction)
+    private Vector2i GetAdjustedIndex(Vector2i index, DirectionFlag direction)
     {
-        switch (direction)
+        return direction switch
         {
-            case Direction.East:
-                return index;
-            case Direction.South:
-                return
-            default:
-                throw new ArgumentOutOfRangeException();
-        }
+            DirectionFlag.East => index,
+            DirectionFlag.South => new Vector2i(index.Y, -index.X),
+            DirectionFlag.West => new Vector2i(-index.X, -index.Y),
+            DirectionFlag.North => new Vector2i(-index.Y, index.X),
+            _ => throw new ArgumentOutOfRangeException()
+        };
+    }
+
+    private Vector2 GetNewLocalPos(Vector2 oldLocalPos, Vector2i origin, DirectionFlag direction)
+    {
+        throw new NotImplementedException();
     }
 }
