6b8bd5260b75f33b5ebc94c252436cfd1c1de453
metalgearsloth <comedian_vs_clown@hotmail.com>
2024-01-07 01:40:37 +1100

    Fix debugconsole logger nullref

--- Robust.Client/UserInterface/CustomControls/DebugConsole.xaml.cs
+++ Robust.Client/UserInterface/CustomControls/DebugConsole.xaml.cs
@@ -39,7 +39,7 @@ namespace Robust.Client.UserInterface.CustomControls
     // And also if Update() stops firing due to an exception loop the console will still work.
     // (At least from the main thread, which is what's throwing the exceptions..)
     [GenerateTypedNameReferences]
-    public sealed partial class DebugConsole : Control, IDebugConsoleView, IPostInjectInit
+    public sealed partial class DebugConsole : Control, IDebugConsoleView
     {
         [Dependency] private readonly IClientConsoleHost _consoleHost = default!;
         [Dependency] private readonly IResourceManager _resourceManager = default!;
@@ -56,6 +56,8 @@ namespace Robust.Client.UserInterface.CustomControls
             RobustXamlLoader.Load(this);
 
             IoCManager.InjectDependencies(this);
+
+            _logger = _logMan.GetSawmill("dbgconsole");
 
             InitCompletions();
 
@@ -281,11 +283,6 @@ namespace Robust.Client.UserInterface.CustomControls
                     writer.Write(newHistory);
                 }
             });
-        }
-
-        void IPostInjectInit.PostInject()
-        {
-            _logger = _logMan.GetSawmill("dbgconsole");
         }
     }
 }
